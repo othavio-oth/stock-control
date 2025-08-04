3c609cec8d3033b1c9135a7f77aa54bacfa5c74c
Othavio <otaviodelimasousa@gmail.com>
2025-08-04 10:29:22 -0300

    fix cors error

--- app/api/main.py
+++ app/api/main.py
@@ -23,7 +23,7 @@ from typing import List
     
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = json.loads(os.getenv("ALLOWED_ORIGINS", "[]")),
+    allow_origins=["https://strategy-web-inventory.vercel.app", "https://strategy-web-inventory-git-master-othaviooths-projects.vercel.app", "https://strategy-web-inventory-dqp5wdm4q-othaviooths-projects.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
