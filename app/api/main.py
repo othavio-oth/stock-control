92b891c5f659dcdb346dfa309e0a06e762c3251a
Othavio <otaviodelimasousa@gmail.com>
2025-09-07 17:46:24 -0300

    add preview

--- app/api/main.py
+++ app/api/main.py
@@ -21,7 +21,7 @@ app.router.redirect_slashes = False
 app.add_middleware(ErrorNotifierMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://strategy-web-inventory.vercel.app", "https://strategy-web-inventory-git-master-othaviooths-projects.vercel.app", "https://strategy-web-inventory-dqp5wdm4q-othaviooths-projects.vercel.app", "http://localhost:3000"],
+    allow_origins=["https://strategy-web-inventory.vercel.app", "https://strategy-web-inventory-git-master-othaviooths-projects.vercel.app", "https://strategy-web-inventory-git-preview-othaviooths-projects.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
