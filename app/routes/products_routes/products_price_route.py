98b66bf4aa521c31ba7642e80079e4e0602febdb
TImoteoVieira <timoteodeveloper@gmail.com>
2026-04-06 16:40:41 -0300

    fix: rename function

--- app/routes/products_routes/products_price_route.py
+++ app/routes/products_routes/products_price_route.py
@@ -23,7 +23,7 @@ def get_current_prices_endpoint(product_id: int, db: Session = Depends(get_db)):
 @router.patch("/{price_id}/", include_in_schema=False)
 @router.patch("/{price_id}")
 def update_price_endpoint(price_id: int, data: ProductPriceHistoryUpdate, db: Session = Depends(get_db)):
-    result = ProductPriceHistoryService.update_price_servicejj(db, price_id, data)
+    result = ProductPriceHistoryService.update_price_service(db, price_id, data)
     if not result:
         raise HTTPException(status_code=404, detail="Price history not found")
     return result
